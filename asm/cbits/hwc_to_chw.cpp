6c3bd358ffc833970a2ad80fe094c96d9e04c857
Abdul Dakkak <abduld@wolfram.com>
2017-08-15 13:23:10 +0100

    work

--- asm/cbits/hwc_to_chw.cpp
+++ asm/cbits/hwc_to_chw.cpp
@@ -1,14 +1,29 @@
+#include <immintrin.h>
 
-static inline int hwc_to_chw(float *output, float *input, int height,
-                             int width) {
-  float *rOutputPlane = &output[0 * width * height];
-  float *bOutputPlane = &output[1 * width * height];
-  float *gOutputPlane = &output[2 * width * height];
+typedef float float3 __attribute__((ext_vector_type(3)));
+
+#define UNROLL_FACTOR 4
+int hwc_to_chw(float *__restrict__ output, const float *__restrict__ input,
+               const int height, const int width) {
+  const int channels = 3;
+  float *const __restrict__ rOutputPlane = &output[0 * width * height];
+  float *const __restrict__ bOutputPlane = &output[1 * width * height];
+  float *const __restrict__ gOutputPlane = &output[2 * width * height];
+  int inputOffset = 0;
+  int outputOffset = 0;
   for (int yy = 0; yy < height; yy++) {
-    for (int xx = 0; xx < width; xx++) {
-      *rOutputPlane = *input++;
-      *bOutputPlane = *input++;
-      *gOutputPlane = *input++;
+    for (int xx = 0; xx < width / UNROLL_FACTOR; xx++) {
+      __builtin_prefetch(&input[inputOffset + channels * UNROLL_FACTOR], 0, 1);
+#pragma unroll
+      for (int ii = 0; ii < UNROLL_FACTOR; ii++) {
+        const float3 rgb{input[inputOffset], input[inputOffset + 1],
+                         input[inputOffset + 2]};
+        rOutputPlane[outputOffset] = rgb.x;
+        bOutputPlane[outputOffset] = rgb.y;
+        gOutputPlane[outputOffset] = rgb.z;
+        inputOffset += channels;
+        outputOffset++;
+      }
     }
   }
   return 0;
