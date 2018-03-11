90453c0f1f02f84b9e8cd11657bef0092a413736
Abdul Dakkak <abduld@wolfram.com>
2018-03-11 00:33:10 -0600

    add options to use jpeg

--- jpeg.go
+++ jpeg.go
@@ -5,32 +5,44 @@ package image
 import (
 	"image"
 	"image/gif"
+	"image/jpeg"
 	"image/png"
 	"io"
+	"runtime"
 
 	"github.com/pkg/errors"
-	"github.com/rai-project/go-libjpeg"
+	libjpeg "github.com/rai-project/go-libjpeg"
 	"golang.org/x/image/bmp"
 )
 
+func jpegDecoder() func(io.Reader) (image.Image, error) {
+	// if runtime.GOARCH == "ppc64le" {
+	// 	return jpeg.Decode
+	// }
+	return func(r io.Reader) (image.Image, error) {
+		return libjpeg.Decode(r, &libjpeg.DecoderOptions{
+			DisableFancyUpsampling: true,
+		})
+	}
+}
+
 func getDecoder(format string, options *Options) (func(io.Reader) (image.Image, error), error) {
 	if format == "jpeg" && options.resizeWidth != 0 && options.resizeHeight != 0 {
+		if runtime.GOARCH == "ppc64le" {
+			return jpeg.Decode, nil
+		}
 		return func(r io.Reader) (image.Image, error) {
-			return jpeg.Decode(r, &jpeg.DecoderOptions{
+			return libjpeg.Decode(r, &libjpeg.DecoderOptions{
 				ScaleTarget:            image.Rect(0, 0, options.resizeWidth, options.resizeHeight),
 				DisableFancyUpsampling: true,
 			})
 		}, nil
 	}
 	imageFormatDecoders := map[string]func(io.Reader) (image.Image, error){
-		"jpeg": func(r io.Reader) (image.Image, error) {
-			return jpeg.Decode(r, &jpeg.DecoderOptions{
-				DisableFancyUpsampling: true,
-			})
-		},
-		"png": png.Decode,
-		"gif": gif.Decode,
-		"bmp": bmp.Decode,
+		"jpeg": jpegDecoder(),
+		"png":  png.Decode,
+		"gif":  gif.Decode,
+		"bmp":  bmp.Decode,
 	}
 	if decoder, ok := imageFormatDecoders[format]; ok {
 		return decoder, nil
